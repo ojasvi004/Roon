4ac0ba263ebf265fc0751bafce5959cfd95907ce
Ojasvi <ojasvidoye@gmail.com>
2025-01-04 11:24:25 +0530

    middleware update

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,12 @@
 import { NextResponse } from 'next/server';
-import { getToken } from 'next-auth/jwt';
 import type { NextRequest } from 'next/server';
 
-export async function middleware(request: NextRequest) {
+export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
+
   const isPublicPath = path === '/' || path === '/register';
 
-  const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
+  const token = request.cookies.get('next-auth.session-token')?.value || '';
 
   if (isPublicPath && token) {
     return NextResponse.redirect(new URL('/chats', request.nextUrl));
